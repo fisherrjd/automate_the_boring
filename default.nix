47f48b3fbcf8af13dc3b61d72c671d550c6640dc
fisherrjd <fisherrjd@gmail.com>
2023-03-01 01:02:03 +0000

    automate the boring stuff intit

---
+++ default.nix
@@ -0,0 +1,33 @@
+{ jacobi ? import
+    (fetchTarball {
+      name = "jacobi-2023-02-25";
+      url = "https://nix.cobi.dev/x/4d4f5946e862bae2391493f471555c43cd753e99";
+      sha256 = "00jwbhf1fn5xsmcybna6401mk011mn17zasz6qsngzzwdlcfb6kh";
+    })
+    { }
+}:
+let
+  name = "automate_the_boring";
+
+
+  tools = with jacobi; {
+    cli = [
+      coreutils
+      nixpkgs-fmt
+    ];
+    python = [
+      (python310.withPackages (p: with p; [
+        requests
+      ]))
+    ];
+    scripts = [ ];
+  };
+
+  env = let paths = jacobi._toolset tools; in
+    jacobi.buildEnv {
+      inherit name;
+      buildInputs = paths;
+      paths = paths;
+    };
+in
+env
---
+++ main.py
@@ -0,0 +1,3 @@
+spam = ["cat", "bat", "rat", "elephant"]
+print(spam[-3])
+print("The " + spam[-1] + " is afraid of the " + spam[-3] + ".")
